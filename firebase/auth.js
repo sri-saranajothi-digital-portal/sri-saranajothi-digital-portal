c7dbd4c3c1be44161cb9ed7afd5e1b55e7f1ebe1
sri-saranajothi-digital-portal <srisaranajothidahampasala@gmail.com>
2026-07-23 21:40:49 +0530

    Update auth.js

--- firebase/auth.js
+++ firebase/auth.js
@@ -1,1 +1,7 @@
+import { app } from "./firebase-config.js";
 
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+const auth = getAuth(app);
+
+export { auth };
