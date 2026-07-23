11b347e46b1b0039b36da9acc5b8305c8d06379c
sri-saranajothi-digital-portal <srisaranajothidahampasala@gmail.com>
2026-07-23 21:37:27 +0530

    Update login.js

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,1 +1,25 @@
+import { auth } from "../../firebase/auth.js";
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
+const loginForm = document.getElementById("loginForm");
+const message = document.getElementById("message");
+
+loginForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    try {
+        await signInWithEmailAndPassword(auth, email, password);
+
+        message.style.color = "green";
+        message.innerText = "Login Successful...";
+
+        window.location.href = "index.html";
+
+    } catch (error) {
+        message.style.color = "red";
+        message.innerText = error.message;
+    }
+});
